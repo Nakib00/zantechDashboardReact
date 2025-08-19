b6c81f2086ef7909ca65a9c9d368d76f81d82117
Nakib00 <2022142@iub.edu.bd>
2025-08-20 04:31:28 +0600

    f

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://hubbackend.desklago.com/api';
+const BASE_URL = 'https://zantechbackend.desklago.com/api';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
